0d1811ee2332b1fd64c93191d4ce192fef04d853
Nathalia Paiva Lima <npl@ug2c30.windows.cin.ufpe.br>
2018-05-15 11:49:12 -0300

    estados da unidade de controle

--- Control.sv
+++ Control.sv
@@ -3,6 +3,10 @@ module Control (
 );
 
 enum logic [meme:mene] {
+
+	//Our Control Unit looks like an H
+
+	//dentro do H - superior centro
 	Reset=
 	Fetch=
 	Wait=
@@ -33,6 +37,79 @@ enum logic [meme:mene] {
 	LB_wait=
 	LB_write=
 	SH_address=
+	SH_memory=
+	SH_wait
+	SH_write=
+	SB_address=
+	SB_memory=
+	SB_wait
+	SB_write=
+	Slti=
+	RegT_Write_slti=
+	Addiu=
+	Addi=
+	RegT_Write=
+
+	//dentro do H -  inferior - esquerdo
+
+	noop_load=
+	noop_wait=
+	noop_pc=
+
+	//dentro do H  - inferior - direito
+
+	Push=
+	NewRA= //New_$RA
+	SaveRA_write= //Save_$RA & WriteMem
+
+	Pop=
+	Address= //endereço
+	readAddres= //ler endereço
+	Stall=
+	Save_Reg= //guarda no registrador
+	New_pointer//novo ponteiro
+	UpdateRA= //Atualiza $ra
+
+	Rte= 
+
+	//lado direito
+
+	Mflo=
+	Mfhi=
+	Jr=
+	Break=
+	Slt=
+	RegD_write_slt=
+	SUb=
+	And=
+	Add=
+	RegD_write=
+
+	ShiftShamt=
+	sll=
+	srl=
+	sra=
+
+	ShiftShamt=
+	sllv=
+	srav=
+
+	RegD_Shift=
+
+	Div=
+	HliLoD=
+	Mult=
+	HliLoM=
+
+	//dentro de H - superior - direito
+
+	Div_Zero=
+	Div_Stall=
+	noop_load=
+	noop_wait=
+	noop_pc=
+
+
 }
 
 endmodule: Control
