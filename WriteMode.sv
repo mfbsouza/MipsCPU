82eb5fc03de85e448c09ad6dcb2f407961f5fbe7
Matheus Ferreira <mfbs@cin.ufpe.br>
2018-05-17 17:57:17 -0300

    fixed letter

--- WriteMode.sv
+++ WriteMode.sv
@@ -3,7 +3,7 @@ module WriteMode(input logic [31:0] Address, input logic [31:0] RegIn, input log
 logic [31:0] MRB, MRH, RMB, RMH;
 logic [1:0] sel;
 
-mux4 mux_RegOUt(.A(MemIn), .B({16'b0, MemIn[15:0]}), .C({24'b0, MemIn[7:0]}), .S(RegOut), .sel(mode[1:0]));
-mux4 mux_MemOut(.A(RegIn), .B({MemIn[31:16], RegIn[15:0]}), .C({MemIn[31:8], RegIn[7:0]}), .S(MemOut), .sel(mode[1:0]));
+Mux4 mux_RegOUt(.A(MemIn), .B({16'b0, MemIn[15:0]}), .C({24'b0, MemIn[7:0]}), .S(RegOut), .sel(mode[1:0]));
+Mux4 mux_MemOut(.A(RegIn), .B({MemIn[31:16], RegIn[15:0]}), .C({MemIn[31:8], RegIn[7:0]}), .S(MemOut), .sel(mode[1:0]));
 
 endmodule: WriteMode
